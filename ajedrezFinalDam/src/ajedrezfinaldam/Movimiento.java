b5c9e7840e2c39872fc901365e99267167a068df
KIEF_ <KIEF_@13KIEF13>
2018-02-14 13:00:14 +0100

    v 1.0

---
+++ ajedrezFinalDam/src/ajedrezfinaldam/Movimiento.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrezfinaldam;
+
+/**
+ *
+ * @author KIEF_
+ */
+public class Movimiento {
+    
+  //Atributos
+   
+    Posicion posInicial, posFinal;
+    
+    
+    
+  //Constructores
+   
+    public Posicion movimiento (Posicion posInicial,Posicion posFinal){
+        return this.posFinal;
+        
+        
+    }
+    
+    
+    //Metodos
+    
+    public boolean esVertical (){
+        return true;
+    }
+    
+    
+    public boolean esHorizontal(){
+        return true;
+    }
+    
+    
+    public boolean esDiagonal(){
+        return true;
+    }
+    
+    
+    //  saltoHorizontal():devuelve la cantidad de salto horizontal           saltoVertical():devuelve la cantidad de salto vertical
+
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ ajedrezFinalDam/src/ajedrezfinaldam/Posicion.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrezfinaldam;
+
+/**
+ *
+ * @author KIEF_
+ */
+
+
+public class Posicion {
+    
+    //Atributos
+    int fila, columna;
+    
+    
+    //constructores
+    public Posicion (){
+        
+        
+   }
+    
+    
+    //getters y setters   
+    public int getFila(){
+        
+       return fila;
+    }
+    
+    
+    public int getColumna (){
+        
+        return columna;
+    }
+    
+    
+    public void setFila (){
+        
+        
+    }
+    
+    public void setColumna (){
+        
+        
+    }
+    
+    //String
+    public String toString (){
+        
+        return " estoy en fila " + fila + " y columna " + columna ;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
